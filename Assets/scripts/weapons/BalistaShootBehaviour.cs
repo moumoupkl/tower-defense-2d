058255850c521bd62fa11f55b24e428119e1969a
mistertyty <loic.henaut@orange.fr>
2024-10-10 15:49:21 +0200

    working non rotating turret with shoot to closest ennemy

--- Assets/scripts/weapons/BalistaShootBehaviour.cs
+++ Assets/scripts/weapons/BalistaShootBehaviour.cs
@@ -5,20 +5,29 @@ public class BallistaShootBehavior : ShootBehavior
     public GameObject boltPrefab;
     public Transform firePoint;
     public float boltSpeed;
+    public Transform target;
     private CDDamageDower cddamagetower;
+    private TurretController TurretController;
 
     private void Start()
     {
+        TurretController = GetComponent<TurretController>();
         cddamagetower = GetComponent<CDDamageDower>();
     }
+
     public override void Shoot()
     {
         // Instantiate and shoot bolt
+        target = TurretController.targetEnemy;
         Debug.Log("shoot");
-        GameObject bolt = Instantiate(boltPrefab, firePoint.position, firePoint.rotation);
+        GameObject bolt = Instantiate(boltPrefab, firePoint.position, Quaternion.identity);
         bolt.GetComponent<Bullet>().damage = cddamagetower.damage;
         Rigidbody2D rb = bolt.GetComponent<Rigidbody2D>();
-        rb.velocity = -firePoint.right * boltSpeed;
+
+        // Calculate direction and apply velocity
+        Vector2 direction = (target.position - firePoint.position).normalized;
+        rb.velocity = direction * boltSpeed;
+
         FindObjectOfType<AudioManager>().Play("Ballista_reload");
         FindObjectOfType<AudioManager>().Play("Ballista_release");
     }
