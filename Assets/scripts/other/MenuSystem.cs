04f821caf07db182b6ebca5568ced9753b1b07da
cesarDesmarets <ps3000@ymail.com>
2024-10-11 09:22:38 +0200

    menu open and close on button press lets gooooo

--- Assets/scripts/other/MenuSystem.cs
+++ Assets/scripts/other/MenuSystem.cs
@@ -9,13 +9,11 @@ public class MenuSystem : MonoBehaviour
     //list of the 8 panels of the selection menu
     public GameObject[] panels;
     public InputActionReference openMenu;
-    private bool open;
 
     void Start()
     {
         //hide the selection menu
         selectionMenu.SetActive(false);
-        
 
         //populate the panels array with the x panels of the selection menu
         for (int i = 0; i < panels.Length; i++)
@@ -28,39 +26,16 @@ public class MenuSystem : MonoBehaviour
 
     void Update()
     {
-        //if the openMenu action is triggered, set open to true
+        //if openMenu is held down, show the selection menu
         if (openMenu.action.triggered)
         {
-            SetPanelRenderers(true);
-            SetPlayerInput(true);
-            open = true;
+            selectionMenu.SetActive(true);
         }
-        /*
-        //if the openMenu action is let go, set open to false
-        if (openMenu.action.phase == InputActionPhase.Canceled)
+        
+        if (openMenu.action.WasReleasedThisFrame())
         {
-            SetPanelRenderers(false);
-            SetPlayerInput(false);
-            open = false;
+            selectionMenu.SetActive(false);
         }
-        */
-    }
-
-    //function to set player_input bool of the animator to true/false for every panel
-    public void SetPlayerInput(bool input)
-    {
-        for (int i = 0; i < panels.Length; i++)
-        {
-            panels[i].GetComponent<Animator>().SetBool("player_input", input);
-        }
-    }
-
-    //function to activate/deactivate the renderers of the panels
-    public void SetPanelRenderers(bool render)
-    {
-        for (int i = 0; i < panels.Length; i++)
-        {
-            panels[i].GetComponent<Renderer>().enabled = render;
-        }
+        
     }
 }
