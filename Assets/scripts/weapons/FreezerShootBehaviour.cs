ebae3c667cd058b13db146bf9089491dac47b1fb
mistertyty <loic.henaut@orange.fr>
2024-10-15 10:33:24 +0200

    ahhhhhhhh redoing everything for the freezer

--- Assets/scripts/weapons/FreezerShootBehaviour.cs
+++ Assets/scripts/weapons/FreezerShootBehaviour.cs
@@ -8,7 +8,7 @@ public class FreezerShootBehaviour : ShootBehavior
     public float slowDuration;
     public float slowStrength;
     private TurretController TurretController;
-        public Transform target;
+    public Transform target;
 
     public override void Shoot()
     {
@@ -19,21 +19,5 @@ public class FreezerShootBehaviour : ShootBehavior
         Vector3 dir = target.position - transform.position;
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-
-
-        foreach (var collider in triangRange.colliders)
-        {
-            // Get the GameObject that the collider is attached to
-            GameObject colliderObject = collider.gameObject;
-
-            // Try to get the script you want to affect (replace 'YourScript' with the actual script name)
-            TroupMovement troupMovement = colliderObject.GetComponent<TroupMovement>();
-
-            if (troupMovement != null)
-            {
-                // Perform your action on the script
-                troupMovement.Slow(slowDuration, slowStrength); // Replace with the method you want to call
-            }
-        }
     }
 }
